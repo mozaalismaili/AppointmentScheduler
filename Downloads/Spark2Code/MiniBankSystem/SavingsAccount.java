a04b034903ceda2cd98a85b4311fb2faa63d152a
reem khalifa <aladawi12reem6@gmail.com>
2025-07-22 00:05:15 +0400

    Applied inheritance: SavingsAccount extends BankAccount with interest feature

---
+++ Downloads/Spark2Code/MiniBankSystem/SavingsAccount.java
@@ -0,0 +1,15 @@
+
+public class SavingsAccount extends BankAccount{
+    private double interestRate;
+
+    public SavingsAccount(String accountName, String accountNumber, double balance, double interestRate) {
+        super(accountName, accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    public void applyInterest() {
+        double interest = balance * interestRate / 100;
+        balance += interest;
+        System.out.println("Interest applied:" + interest + ". New Balance:" + balance);
+    }
+}
