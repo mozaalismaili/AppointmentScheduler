9ed8664a8e264c3729dc4cf401489ce07d6ce9b1
reem khalifa <aladawi12reem6@gmail.com>
2025-07-21 23:36:54 +0400

    Add menu to console app

---
+++ Downloads/Spark2Code/MiniBankSystem/BankSystem.java
@@ -0,0 +1,16 @@
+public class BankSystem {
+    public static void main(String[] args){
+
+
+
+    }
+    public static void DisplayMenu(){
+
+        System.out.println("Option 1: Create a new account");
+        System.out.println("Option 2: Deposit money");
+        System.out.println("Option 3: Withdraw money");
+        System.out.println("Option 4: View account details");
+        System.out.println("Option 5: Exit");
+    }
+
+}
