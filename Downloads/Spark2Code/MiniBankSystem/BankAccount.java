735071707089b77f387988c6bfed4ee5b684bc77
reem khalifa <aladawi12reem6@gmail.com>
2025-07-21 23:38:58 +0400

    Create BankAccount class with constructor

--- Downloads/Spark2Code/MiniBankSystem/BankAccount.java
+++ Downloads/Spark2Code/MiniBankSystem/BankAccount.java
@@ -1,4 +1,28 @@
 public class BankAccount {
-
+    String accountName;
+    String accountNumber;
+    double balance;
+    //Add a constructor
+    public BankAccount(String accountName, String accountNumber, double balance) {
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+    public void showAccountInfo(){
+        System.out.println("Account Name:"+" "+accountName);
+        System.out.println("Account Number"+" "+accountNumber);
+        System.out.println("Balance"+" "+balance);
+    }
+    public void Desposit(double amount){
+        balance += amount;
+    }
+    public void Withdraw(double amount){
+       if (amount>balance){
+           System.out.println("non-sufficient funds");       }
+       else{
+           balance -= amount;
+           System.out.println("Your account balance:"+" "+balance);
+       }
+    }
 
 }
