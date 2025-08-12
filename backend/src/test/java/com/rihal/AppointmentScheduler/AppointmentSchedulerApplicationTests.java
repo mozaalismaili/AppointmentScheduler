8f0f0c997cb034d058241365ed1bc6f063c741ca
Al Baraa <albraa9021@gmail.com>
2025-08-12 23:06:24 +0400

    Added project structure for backend

---
+++ backend/src/test/java/com/rihal/AppointmentScheduler/AppointmentSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rihal.AppointmentScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
